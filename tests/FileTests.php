8f4756251342ffdeb506c80daaa6367415fee466
Phillip Jackson <unisys12@gmail.com>
2014-07-05 18:12:54 -0500

    correct file so that it will pass validation

--- tests/FileTests.php
+++ tests/FileTests.php
@@ -1,7 +1,12 @@
 <?php namespace PHillipJackson\Tests\FileTests
 
+use PHillipJackson\AssHatMan\File as FileInfo;
+
 class FileTests extends PHPUnit_Framework_TestCase {
 
-
+	public function testRetrieveingAFile()
+	{
+		$file = new FileInfo();
+	} 
 
 }
