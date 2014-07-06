039e06543a1212d38dd2e3d3dad67f92af89c2ca
Phillip Jackson <unisys12@gmail.com>
2014-07-05 23:26:07 -0500

    getting started

--- src/File.php
+++ src/File.php
@@ -5,11 +5,21 @@ class File {
 	public function __construct($file)
 	{
 		$this->file = $file;
-	}	
+	}
 
-	public function getFile($file)
+	public function directoryList()
+	{
+		return array_diff(scandir($this->file), array('..', '.'));
+	}
+
+	public function getFile()
 	{
 		return $this->file;
 	}
+
+	public function getFileNames()
+	{
+		return $this->directoryList();
+	}	
 
 }
--- tests/FileTests.php
+++ tests/FileTests.php
@@ -1,12 +1,39 @@
-<?php namespace PHillipJackson\Tests\FileTests
+<?php
 
-use PHillipJackson\AssHatMan\File as FileInfo;
+use PhillipJackson\AssHatMan\File;
 
-class FileTests extends PHPUnit_Framework_TestCase {
+class FileTests extends PHPUnit_Framework_TestCase{
+
+	public function setUp()
+	{
+
+	}
+
+	public function testThatWeCanScanAGivenDirectory()
+	{
+		$dirtocheck = __DIR__ . '/../public/css/';
+		
+		$file = new File($dirtocheck);
+		$listing = $file->directoryList();
+		$this->assertNotEmpty($listing);
+	}
 
 	public function testRetrieveingAFile()
+	{		
+		$filetocheck = file(__DIR__ . '/../public/css/testcss.css');
+
+		$file = new File($filetocheck);
+		$checkfile = $file->getFile($filetocheck);
+		$this->assertNotEmpty($checkfile);
+	}
+
+	public function testThatWeCanRetrieveAFileName()
 	{
-		$file = new FileInfo();
-	} 
+		$filetocheck = __DIR__ . '/../public/css/';
+
+		$dir = new File($filetocheck);
+		$checkforname = $dir->getFileNames();
+		$this->assertContains('testcss.css', $checkforname);
+	}
 
 }
